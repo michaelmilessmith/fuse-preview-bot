be86fab07546a3600b75fd7d9643e7976aa7b4b6
Michael Smith <michael.smith@fuseuniversal.com>
2018-09-07 09:59:04 +0100

    Make it better

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ app.use(bodyParser.json())
 app.post('/', function (req, res) {
   console.log(req.body)
 })
+
+const port = process.env.PORT || 3004;
+
+app.listen(port, () => {
+    console.info(`Server is up on ${port}`);
+});
