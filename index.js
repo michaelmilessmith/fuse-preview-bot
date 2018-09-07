c93e8e4bf77d936be8f4a5b5e422d63f94c7718d
Michael Smith <michael.smith@fuseuniversal.com>
2018-09-07 10:23:22 +0100

    Make it better

--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
+const request = require('superagent')
 
 app.use(bodyParser.json())
 
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
   console.log(req.body)
-  if(req.body.challenge) {
+  if (req.body.challenge) {
     res.status(200).send(req.body.challenge)
     return
+  }
+  if (req.body.event.links) {
+    console.log(req.body.event.links)
+    const { token, event: { channel, links, message_ts: ts } } = req.body
+    request
+      .post('https://slack.com/api/chat.unfurl')
+      .send({
+        token,
+        channel,
+        ts,
+        unfurls: {
+          'https://fuse.fuseuniversal.com/': {
+            text: 'Every day is the test.'
+          }
+        },
+        user_auth_required: true
+      })
+      .then(res => console.log(res))
   }
   res.status(200).send()
 })
 
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3004
 
 app.listen(port, () => {
-    console.info(`Server is up on ${port}`);
-});
+  console.info(`Server is up on ${port}`)
+})
